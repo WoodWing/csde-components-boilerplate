5af188fa7e6ae4aabbdf9b18179419128a5852c6
Sander van Noort <sno@woodwing.com>
2021-12-30 13:52:19 +0100

    Move defaults component set to `component-sets/default-components` and support building mulitple component sets.
    - Remove gulp in favor of new mjs build script. Gulp is not adding enough and not maintained.
    - Remove watch/dev mode for now.

---
+++ build.mjs
@@ -0,0 +1,168 @@
+import fs from 'node:fs/promises';
+import fsCallback from 'node:fs';
+import path from 'path';
+import sass from 'sass';
+import UglifyJS from 'uglify-js';
+import archiver from 'archiver';
+import { validateFolder } from '@woodwing/studio-component-set-tools/dist/validate.js';
+
+const componentSetsDirectory = 'component-sets';
+
+/**
+ * Walk through all folders in componentSetsDirectory and build them.
+ */
+async function buildComponentSets() {
+    for (const fileName of await fs.readdir(componentSetsDirectory)) {
+        const componentSetPath = path.join(componentSetsDirectory, fileName);
+        if (!(await fs.stat(componentSetPath)).isDirectory()) continue;
+        await buildComponentSet(componentSetPath);
+    }
+}
+
+/**
+ * Build a component set zip for path.
+ * @param {string} componentSetPath 
+ */
+async function buildComponentSet(componentSetPath) {
+    console.log(`Building component set "${componentSetPath}"`);
+    await generateDesignFile(componentSetPath);
+    await compileDesignFile(componentSetPath);
+    await generateVendorScript(componentSetPath);
+    await validateComponentSet(componentSetPath);
+    await buildComponentSetZip(componentSetPath);
+}
+
+/**
+ * Runs component set validation and throws error when something is wrong.
+ * @param {string} componentSetPath 
+ */
+ async function validateComponentSet(componentSetPath) {
+    console.log(`Validating component set "${componentSetPath}"`);
+    const valid = await validateFolder(componentSetPath);
+    if (!valid) {
+        throw new Error(`Package failed validation: "${componentSetPath}". See errors above.`);
+    }
+}
+
+/**
+ * Generates design.scss from style files.
+ * @param {string} componentSetPath 
+ */
+ async function generateDesignFile(componentSetPath) {
+    console.log(`Generating design.scss for component set "${componentSetPath}"`);
+    try {
+        const definition = await getComponentDefinition(componentSetPath);
+        const componentNames = definition.components.map((compDef) => compDef.name);
+
+        let content = `/*
+* This file has been generated while building the components package.
+* PLEASE DO NOT MODIFY THIS FILE BY HAND.
+*/\n${buildScssString('common')}`;
+        
+        for (const componentName of componentNames) {
+            content += buildScssString(componentName);
+        }
+    
+        await fs.writeFile(path.join(componentSetPath, 'styles/design.scss'), content);
+    } catch (e) {
+        // Don't fail task, but let the components validator generate the actual error,
+        // in case the component definition is malformed.
+        console.error('Failed to generate design.scss file: \n', e);
+        return;
+    }
+}
+/**
+ * @param {string} componentSetPath 
+ */
+async function getComponentDefinition(componentSetPath) {
+    return JSON.parse(await fs.readFile(path.join(componentSetPath, 'components-definition.json'), 'utf8'));
+}
+
+/**
+ * @param {string} componentName 
+ * @returns {string}
+ */
+function buildScssString(componentName) {
+    return `@import "${componentName}";\n`;
+}
+
+/**
+ * Compiles design.scss.
+ * @param {string} componentSetPath 
+ */
+ async function compileDesignFile(componentSetPath) {
+    console.log(`Compiling design.scss for component set "${componentSetPath}"`);
+    const result = sass.compile(path.join(componentSetPath, 'styles/design.scss'));
+    await fs.writeFile(path.join(componentSetPath, 'styles/design.css'), result.css);
+}
+
+/**
+ * Generates vendor script by concatenating and uglifying the result.
+ * @param {string} componentSetPath 
+ */
+ async function generateVendorScript(componentSetPath) {
+    console.log(`Generating vendor.js for component set "${componentSetPath}"`);
+
+    // Scripts bundled into a single vendor.js
+    // You can choose to remove scripts in case they are not needed
+    // for your components.
+    const scriptsDir = path.join('./scripts');
+    const scriptFiles = [
+        // JQuery libraries used by below support scripts
+        path.join(scriptsDir, 'jquery.js'),
+
+        // Support scripts for slideshows components
+        path.join(scriptsDir, 'jssor.js'),
+        path.join(scriptsDir, 'jssor.slider.js'),
+        path.join(scriptsDir, 'slideshow.js'),
+
+        // Support script for parallax effect hero components
+        path.join(scriptsDir, 'heroes.js'),
+    ];
+
+    // Concat files
+    let content = '';
+    for (const scriptFile of scriptFiles) {
+        content += `${(await fs.readFile(scriptFile)).toString()}\n`;
+    }
+    // Uglify result
+    const result = UglifyJS.minify(content);
+    if (result.error) {
+        throw new Error(result.error.message);
+    }
+    // Write to vendor.js script
+    const targetFolder = path.join(componentSetPath, 'scripts');
+    await mkdirIfNotExists(targetFolder);
+    await fs.writeFile(path.join(targetFolder, 'vendor.js'), result.code);
+}
+
+/**
+ * Creates component set zip.
+ * @param {string} componentSetPath 
+ */
+async function buildComponentSetZip(componentSetPath) {
+    const outputPath = path.join('dist', `${path.basename(componentSetPath)}.zip`);
+    console.log(`Zipping "${outputPath}" component set "${componentSetPath}"`);
+
+    await mkdirIfNotExists('dist');
+
+    const output = fsCallback.createWriteStream(outputPath);
+    const archive = archiver('zip', {
+        zlib: { level: 9 } // Sets the compression level.
+    });
+    archive.pipe(output);
+    archive.directory(componentSetPath, false);
+    await archive.finalize();
+}
+
+async function mkdirIfNotExists(dirPath) {
+    try {
+        await fs.mkdir(dirPath);;
+    } catch(err) {
+        if( err.code != 'EEXIST' ) {
+            throw err;
+        }
+    }
+}
+
+await buildComponentSets();
